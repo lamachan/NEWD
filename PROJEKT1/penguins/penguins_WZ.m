efd909571166fbada27823f87d21454aa0a95416
lamachan <95586117+lamachan@users.noreply.github.com>
2024-03-21 01:19:15 +0100

    Exported plots + some new ones

--- PROJEKT1/penguins/penguins_WZ.m
+++ PROJEKT1/penguins/penguins_WZ.m
@@ -46,10 +46,10 @@ end
 
 disp(counts);
 
+colors = [0.8500 0.3250 0.0980;  % czerwony - Adelie
+          0.9290 0.6940 0.1250;  % żółty - Chinstrap
+          0 0.4470 0.7410];      % niebieski - Gentoo
 figure;
-colors = [0.8500 0.3250 0.0980;  % Red for Adelie Penguin
-          0.9290 0.6940 0.1250;  % Yellow for Chinstrap Penguin
-          0 0.4470 0.7410];      % Blue for Gentoo Penguin
 
 bar(counts', 'stacked');
 
@@ -63,7 +63,7 @@ xticklabels(unique_islands);
 xlabel('Wyspy');
 ylabel('Liczba pingwinów');
 legend(unique_species, 'Location', 'eastoutside', 'Orientation', 'vertical');
-title('Liczba pingwinów na każdej wyspie wg gatunku');
+title('Liczba pingwinów na każdej wyspie wg gatunku', 'FontSize', 20);
 xtickangle(45);
 grid on;
 
@@ -79,10 +79,10 @@ hold off;
 xlabel('Długość płetwy (mm)');
 ylabel('Długość dzioba (mm)');
 legend(unique_species, 'Location', 'best');
-title('Zależność długości dzioba od długości płetwy wg gatunku');
+title('Zależność długości dzioba od długości płetwy wg gatunku', 'FontSize', 20);
 grid on;
 
-% wykres punktowy zależności masy ciała od długości płetwy
+% wykres punktowy zależności masy ciała od długości płetwy wg gatunku
 figure;
 hold on;
 for i = 1:numel(unique_species)
@@ -94,5 +94,35 @@ hold off;
 xlabel('Długość płetwy (mm)');
 ylabel('Masa ciała (g)');
 legend(unique_species, 'Location', 'best');
-title('Zależność masy ciała od długości płetwy wg gatunku');
+title('Zależność masy ciała od długości płetwy wg gatunku', 'FontSize', 20);
+grid on;
+
+% wykres punktowy zależności masy ciała od długości dzioba wg gatunku
+figure;
+hold on;
+for i = 1:numel(unique_species)
+    species_indices = strcmp(species, unique_species{i});
+    scatter(data_table.CulmenLength_mm_(species_indices), data_table.BodyMass_g_(species_indices), 50, colors(i, :), 'filled');
+end
+hold off;
+
+xlabel('Długość dzioba (mm)');
+ylabel('Masa ciała (g)');
+legend(unique_species, 'Location', 'best');
+title('Zależność masy ciała od długości dzioba wg gatunku', 'FontSize', 20);
+grid on;
+
+% wykres punktowy zależności grubości dzioba od długości dzioba wg gatunku
+figure;
+hold on;
+for i = 1:numel(unique_species)
+    species_indices = strcmp(species, unique_species{i});
+    scatter(data_table.CulmenLength_mm_(species_indices), data_table.CulmenDepth_mm_(species_indices), 50, colors(i, :), 'filled');
+end
+hold off;
+
+xlabel('Długość dzioba (mm)');
+ylabel('Grubość dzioba (mm)');
+legend(unique_species, 'Location', 'best');
+title('Zależność grubości dzioba od długości dzioba wg gatunku', 'FontSize', 20);
 grid on;
