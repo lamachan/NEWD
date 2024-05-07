c611d594cc501fe324aec7981b747d0136067fe2
lamachan <95586117+lamachan@users.noreply.github.com>
2024-05-08 00:50:29 +0200

    more data csv files

--- PROJEKT2_1/WZ_script.R
+++ PROJEKT2_1/WZ_script.R
@@ -1,3 +1,3 @@
 setwd("D:/DOKUMENTY/STUDIA/SEMESTR 6/NEWD/PROJEKT/NEWD/PROJEKT2_1")
-data <- read.table("data/wyprawa_polocka.txt", header = TRUE, sep = ';')
+data <- read.table("data/armia_krajowa_1944.txt", header = TRUE, sep = ';', colClasses = c("character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 head(data)
