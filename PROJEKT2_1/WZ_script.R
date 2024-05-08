e6fc4e4d148c429936ed4a7b2786bc17f45bf51b
lamachan <95586117+lamachan@users.noreply.github.com>
2024-05-08 15:43:37 +0200

    even more csv files

--- PROJEKT2_1/WZ_script.R
+++ PROJEKT2_1/WZ_script.R
@@ -1,3 +1,17 @@
+library(ggplot2)
+library(reshape2)
+
 setwd("D:/DOKUMENTY/STUDIA/SEMESTR 6/NEWD/PROJEKT/NEWD/PROJEKT2_1")
-data <- read.table("data/armia_krajowa_1944.txt", header = TRUE, sep = ';', colClasses = c("character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+data <- read.table("data/krzyzacy.txt", header = TRUE, sep = ';', colClasses = c("character", "numeric", "numeric", "numeric"))
 head(data)
+
+data_long <- melt(data, id.vars = "Kwartały", variable.name = "Rodzaj", value.name = "Liczba")
+head(data_long)
+
+ggplot(data_long, aes(x = Kwartały, y = Liczba, fill = Rodzaj)) +
+  geom_bar(stat = "identity", position = "dodge", color = "black") +
+  labs(title = "Liczebność Armii Polskiej w wojnie z Krzyżakami w 1520 r. ",
+       x = "Kwartał",
+       y = "Liczba",
+       fill = "Rodzaj") +
+  theme_minimal()
